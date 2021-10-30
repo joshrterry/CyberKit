8f0d6827608cada31e6249106cb2a667366107b2
J2K05 <70606453+J2K05@users.noreply.github.com>
2021-10-30 15:44:27 -0700

    Add files via upload

--- j2kcypat.sh
+++ j2kcypat.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+addingUsers = 0
+
 function cont() {
     echo Press ENTER to continue.
     read
+}
+
+function promptYN() {
+    prompt="$1 [Y/n] "
+    if [[ "$1" == "-n" ]]; then
+        prompt="$2 [y/N] "
+    fi
+
+    read -p "$prompt" yn
+
+    if [[ "$1" == "-n" ]]; then
+        if [[ -z "$yn" ]]; then
+            return 1
+        else
+            return `[[ $yn =~ ^[yY]$ ]]`
+        fi
+    else
+        return `[[ $yn =~ ^[yY]?$ ]]`
+    fi
 }
 
 function upgradeAll() {
@@ -10,9 +31,35 @@ function upgradeAll() {
     sudo apt upgrade -yy
 }
 
-# function softwareUpdates() {
+function softwareUpdates() {
+    software-properties-gtk
+    echo("CHANGE THE FOLLOWING SETTING UNDER UPDATES:")
+    echo("Automatically check for updates \e[1;41m Daily \e [0m")
+}
 
-# }
+function checkUsers() {
+    if promptYN -n "enter all users?"; then
+    inputUsers
+    else
+    echo "skipped adding new users";
+    fi
+}
+
+function inputUsers() {
+    
+    echo -n > /home/script/passwds.txt
+    echo -n > /home/script/admins.txt
+
+    echo "type out all users, separated by lines"
+    echo "press ENTER again once you are finished"
+
+    while isReading == true
+
+    
+
+}
+
+}
 
 clear
 
@@ -26,13 +73,14 @@ function selector() {
 echo "Type any of the following numbers to select an action:"
     echo "1. update all packages"
     echo "2. enable automatic software updates"
-
+    echo "3. check users"
     read -p "enter section number: " secnum
 }
 
 selector
 case $secnum in
 1) upgradeAll; selector;
+3) 
 esac
 
 
